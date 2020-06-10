d4fbf8c7bd39a95ab5f4f8ea92ee0a183f30c176
Srdjan Milic <srdjanrmilic@gmail.com>
2020-06-10 13:42:11 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListCardsComponent } from './components/list-cards/list-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListCardsComponent
   ],
   imports: [
     BrowserModule,
