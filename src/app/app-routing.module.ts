2243052d06835e2ad3a399ec9c62be0dfde296ca
Srdjan Milic <srdjanrmilic@gmail.com>
2020-06-12 08:18:16 +0200

    routes configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddCardComponent } from './components/add-card/add-card.component';
+import { ListCardsComponent } from './components/list-cards/list-cards.component';
+import { EditCardComponent } from './components/edit-card/edit-card.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'list-cards', pathMatch: 'full'},
+  {path: 'list-cards', component: ListCardsComponent},
+  {path: 'add-card', component: AddCardComponent},
+  {path: 'edit-card', component: EditCardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
