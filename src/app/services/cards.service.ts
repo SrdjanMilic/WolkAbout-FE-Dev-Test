cf5ada4f0f9007794f0ca2cd735edf202ca0a768
Srdjan Milic <srdjanrmilic@gmail.com>
2020-06-12 08:23:44 +0200

    update: new services

--- src/app/services/cards.service.ts
+++ src/app/services/cards.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Card } from '../models/card.model';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
@@ -10,14 +11,48 @@ export class CardsService {
 
   data: any = [];
 
-  constructor(public httpClient: HttpClient, public snackBar: MatSnackBar) { }
+  constructor(public http: HttpClient, public snackBar: MatSnackBar) { }
 
   listCards() {
-    return this.httpClient
+    return this.http
       .get(this.API_SERVER)
       .subscribe((res: any[]) => {
         this.data = res;
         console.log(this.data);
+      });
+  }
+
+  createCard(card: Card) {
+    return this.http
+      .post(this.API_SERVER, card)
+      .subscribe(() => {
+        this.snackBar.open('Card is created!', '', {
+          duration: 2000,
+        });
+        console.log(`Submitted to database!`);
+      });
+  }
+
+  editCard(card: Card) {
+    return this.http
+      .put(this.API_SERVER, card)
+      .subscribe(() => {
+        this.snackBar.open('Card is updated!', '', {
+          duration: 2000,
+        });
+        console.log(`Submitted to database!`);
+      });
+  }
+
+  deleteCard(id: number) {
+    return this.http
+      .delete(`${this.API_SERVER}/${id}`)
+      .subscribe((res: any[]) => {
+        this.data = this.data.filter((x) => x.id !== id);
+        this.snackBar.open('Card is deleted!', '', {
+          duration: 2000,
+        });
+        console.log(res);
       });
   }
 }
